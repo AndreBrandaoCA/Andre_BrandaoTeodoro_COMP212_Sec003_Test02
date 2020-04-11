c1b7e4b6e2af6e8c4065bf7c67b0069507cef798
André Brandão Teodoro <andrebrandaoca@gmail.com>
2020-04-11 16:58:30 -0400

    Project Structure created, Db added

---
+++ Andre_BrandaoTeodoro_Sec003_Exercise01/BooksManipulation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Andre_BrandaoTeodoro_Sec003_Exercise01
+{
+    public partial class BooksManipulation : Form
+    {
+        public BooksManipulation()
+        {
+            InitializeComponent();
+        }
+    }
+}
