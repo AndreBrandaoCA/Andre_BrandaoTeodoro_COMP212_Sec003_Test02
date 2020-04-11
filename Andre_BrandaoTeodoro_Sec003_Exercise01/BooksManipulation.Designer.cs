e546341524aa872493a8caeae32119eac22673f8
André Brandão Teodoro <andrebrandaoca@gmail.com>
2020-04-11 17:50:49 -0400

    Exercise 01 completed

--- Andre_BrandaoTeodoro_Sec003_Exercise01/BooksManipulation.Designer.cs
+++ Andre_BrandaoTeodoro_Sec003_Exercise01/BooksManipulation.Designer.cs
@@ -28,13 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.queryComboBox = new System.Windows.Forms.ComboBox();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // queryComboBox
+            // 
+            this.queryComboBox.FormattingEnabled = true;
+            this.queryComboBox.Items.AddRange(new object[] {
+            "Author count grouped by Title",
+            "Titles grouped by Author Name sorted by Author"});
+            this.queryComboBox.Location = new System.Drawing.Point(13, 13);
+            this.queryComboBox.Name = "queryComboBox";
+            this.queryComboBox.Size = new System.Drawing.Size(775, 21);
+            this.queryComboBox.TabIndex = 0;
+            this.queryComboBox.SelectedIndexChanged += new System.EventHandler(this.queryComboBox_SelectedIndexChanged);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(13, 41);
+            this.resultTextBox.Multiline = true;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.resultTextBox.Size = new System.Drawing.Size(775, 397);
+            this.resultTextBox.TabIndex = 1;
+            // 
+            // BooksManipulation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.queryComboBox);
+            this.Name = "BooksManipulation";
+            this.Text = "Ex01 - Books";
+            this.Load += new System.EventHandler(this.BooksManipulation_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox queryComboBox;
+        private System.Windows.Forms.TextBox resultTextBox;
     }
 }
 
--- Andre_BrandaoTeodoro_Sec003_Exercise01/BooksManipulation.cs
+++ Andre_BrandaoTeodoro_Sec003_Exercise01/BooksManipulation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BooksDb;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +17,72 @@ namespace Andre_BrandaoTeodoro_Sec003_Exercise01
         {
             InitializeComponent();
         }
+        private BooksDb.BooksEntities dbContext = new BooksDb.BooksEntities();
+
+        private void BooksManipulation_Load(object sender, EventArgs e)
+        {
+            queryComboBox.SelectedIndex = 0;
+        }
+        
+        private void queryComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //clear result textbox for the next query
+            resultTextBox.Clear();
+            #region Author Count
+            // First Query: Get count of author by title, sorted by title.
+            if (queryComboBox.SelectedIndex == 0)
+            {
+                //define query for books and authors ordered by title
+                var authorCountByTitle =
+                    from book in dbContext.Titles
+                    orderby book.Title1
+                    select new
+                    {
+                        Title = book.Title1,
+                        AuthorCount =
+                            from author in book.Authors
+                            orderby author.LastName, author.FirstName
+                            select author.FirstName
+                    };
+                // header
+                resultTextBox.AppendText($"{"Title",-120} {"Number of Authors",-20}");
+                // loop to display all titles and authors
+                foreach (var book in authorCountByTitle)
+                {
+                    resultTextBox.AppendText($"\r\n{book.Title,-120} {book.AuthorCount.Count(),-20}");
+                }
+            }
+            #endregion
+            #region Title
+            // Second Query: Get titles grouped by authors name
+            if (queryComboBox.SelectedIndex == 1)
+            {
+                //define query for authors and its books
+                var titleByAuthor =
+                    from author in dbContext.Authors
+                    orderby author.LastName, author.FirstName
+                    select new
+                    {
+                        AuthorLN = author.LastName,
+                        AuthorFN = author.FirstName,
+                        Titles =
+                            from Title in author.Titles
+                            orderby Title.Title1
+                            select Title.Title1
+                    };
+                // header for the GUI
+                resultTextBox.AppendText($"{"Author and its titles:"}\r\n");
+                // loop to display all authors and its
+                foreach (var author in titleByAuthor)
+                {
+                    resultTextBox.AppendText($"\r\n{author.AuthorLN}, {author.AuthorFN}");
+                    foreach (var book in author.Titles) {
+                        resultTextBox.AppendText($"\r\n\t{book,-20}");
+                    }
+                }
+            }
+            #endregion
+        }
+
     }
 }
